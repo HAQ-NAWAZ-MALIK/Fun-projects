864977be605499035fa7e55c86d34928bd4efb8a
Haq Nawaz Malik {Omar} <86514900+HAQ-NAWAZ-MALIK@users.noreply.github.com>
2024-06-11 21:17:15 +0530

    Update config.py

--- YTube-Tgram-Openai-Voice/config.py
+++ YTube-Tgram-Openai-Voice/config.py
@@ -1,27 +1,39 @@
 import os
 import tempfile
-
 from dotenv import load_dotenv, find_dotenv
+
+# Load environment variables from a .env file
 load_dotenv(find_dotenv())
 
+# Get the OpenAI API key from the environment variables
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
+# Get the credentials from the environment variables
 CREDENTIALS = os.getenv('CREDENTIALS')
+
+# Get the Google Cloud Storage bucket name from the environment variables
 GCP_CLOUD_STORAGE_BUCKET_NAME = os.getenv('GCP_CLOUD_STORAGE_BUCKET_NAME')
 
-ERROR_MESSAGE = 'We are facing an issue, please try after sometimes.'
+# Error message to display when facing an issue
+ERROR_MESSAGE = 'We are facing an issue, please try again later.'
+
+# Audio file format to use
 AUDIO_FILE_FORMAT = 'mp3'
 
+# Type of reply to expect (text or audio)
 REPLY_TYPE = 'text'
 # REPLY_TYPE = 'audio'
 
+# Output directory for storing temporary files
 OUTPUT_DIR = os.path.join(
     tempfile.gettempdir(),
     'telegrambot'
 )
 
+# Create the output directory if it doesn't exist
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
+# File path for storing the credentials
 CREDENTIALS_FILE_PATH = os.path.join(
     OUTPUT_DIR,
     'credentials.json'
